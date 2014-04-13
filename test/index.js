21694f4e7ecf86ed1983cd602c9eb3a68af75c0a
Ionică Bizău <bizauionica@yahoo.com>
2014-04-13 14:39:49 +0300

    Changed the input png

--- test/index.js
+++ test/index.js
@@ -1,9 +1,8 @@
 var ImageToAscii = require ("../index")
   , myImage = new ImageToAscii ({
-        multiplyWidth: 1
     })
   ;
 
-myImage.convert(__dirname + "/heisencat.png", function(err, converted) {
+myImage.convert(__dirname + "/octocat.png", function(err, converted) {
     console.log(err || converted);
 });
