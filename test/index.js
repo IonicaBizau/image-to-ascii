0d699855cff32fd52a42e6d430b25d178c47141c
Ionică Bizău <bizauionica@gmail.com>
2015-09-02 08:55:39 +0300

    Comments

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
+// Dependencies
 var ImageToAscii = require("../lib/index");
 
+// Convert to ascii this image
 ImageToAscii(__dirname + "/octocat.png", function(err, converted) {
     console.log(err || converted);
 });
