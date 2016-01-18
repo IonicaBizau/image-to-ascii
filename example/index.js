7d3f70e9ab9516df9808d8e370838d5cf0e508ae
Ionică Bizău <bizauionica@gmail.com>
2016-01-18 17:44:57 +0200

    Clean up and work in progress

--- example/index.js
+++ example/index.js
@@ -6,4 +6,6 @@ imageToAscii(`${__dirname}/octocat.png`, function(err, converted) {
     console.log(err || converted);
 });
 
-
+imageToAscii(`https://octodex.github.com/images/hanukkat.png`, function(err, converted) {
+    console.log(err || converted);
+});
