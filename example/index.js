eb4610f9fffe5d55e7354c8df2a00681826e15f6
Ionică Bizău <bizauionica@gmail.com>
2016-03-02 13:29:55 +0200

    Simple example

--- example/index.js
+++ example/index.js
@@ -1,11 +1,8 @@
+"use strict";
+
 // Dependencies
 const imageToAscii = require("../lib");
 
-// Convert to ascii this image
-imageToAscii(`${__dirname}/octocat.png`, function(err, converted) {
-    console.log(err || converted);
-});
-
-imageToAscii(`https://octodex.github.com/images/hanukkat.png`, function(err, converted) {
+imageToAscii("https://octodex.github.com/images/jetpacktocat.png", (err, converted) => {
     console.log(err || converted);
 });
